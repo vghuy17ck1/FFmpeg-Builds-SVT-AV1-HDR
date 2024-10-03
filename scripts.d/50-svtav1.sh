bf044584ebfdf68ef971d75fac279c7fef869d3e
Uranite <62639703+Uranite@users.noreply.github.com>
2024-10-03 21:59:21 +0700

    svt-av1-hdr 09600d8

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://github.com/BlueSwordM/svt-av1-psyex.git"
-SCRIPT_COMMIT="fedc23378247b6ab504cbcdfedf428efab9ad452"
+SCRIPT_REPO="https://github.com/juliobbv-p/svt-av1-hdr.git"
+SCRIPT_COMMIT="09600d8a1cac9c6403b8dfc4aec62114ef4677fe"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
@@ -17,7 +17,7 @@ ffbuild_dockerbuild() {
     mkdir build && cd build
 
     cmake -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=ON -DSVT_AV1_LTO=OFF -DCMAKE_CXX_FLAGS_RELEASE="-DNDEBUG -O2" -DCMAKE_C_FLAGS_RELEASE="-DNDEBUG -O2" ..
+        -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_APPS=OFF -DENABLE_AVX512=ON -DSVT_AV1_LTO=OFF -DCMAKE_CXX_FLAGS_RELEASE="-flto -DNDEBUG -O2" -DCMAKE_C_FLAGS_RELEASE="-flto -DNDEBUG -O2" ..
     make -j$(nproc)
     make install DESTDIR="$FFBUILD_DESTDIR"
 }
